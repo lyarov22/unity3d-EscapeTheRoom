53044a89c1989b045a11c4e32b4b7c3b0a274011
Rybka <112930666+Rybk4@users.noreply.github.com>
2023-05-11 09:56:13 +0600

    release 1.1

--- Assets/drags.cs
+++ Assets/drags.cs
@@ -7,11 +7,28 @@ public class drags : MonoBehaviour
 
     float distanse = 3;
     public Camera cam;
+    private Rigidbody rb;
+   
     // Start is called before the first frame update
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
     private void OnMouseDrag()
     {
-        Vector3 mouse = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, distanse);
-        Vector3 objPos = cam.ScreenToWorldPoint (mouse);
-        transform.position = objPos;
+        
+            Vector3 mouse = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, distanse); 
+            Vector3 objPos = cam.ScreenToWorldPoint (mouse);
+            rb.isKinematic = true;
+            rb.useGravity = true;
+            transform.position = objPos;
+   
+        
+    }
+    private void OnMouseDown()
+    {
+        rb.isKinematic = false;
     }
 }
