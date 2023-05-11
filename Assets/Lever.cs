d39573543b2ba257096a5704461d7c292fbdb2b0
Rybka <112930666+Rybk4@users.noreply.github.com>
2023-05-11 20:04:10 +0600

    release 1.3

---
+++ Assets/Lever.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lever : MonoBehaviour
+{
+    public float smooth = 2.0f;
+    public float DoorOpenAngle = 90.0f;
+
+
+
+    private Vector3 defaultRot;
+    private Vector3 openRot;
+    private bool open;
+    private bool enter;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        defaultRot = transform.eulerAngles;
+        openRot = new Vector3(defaultRot.x , defaultRot.y, defaultRot.z+ DoorOpenAngle);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (open)
+        {
+
+            transform.eulerAngles = Vector3.Slerp(transform.eulerAngles, openRot, Time.deltaTime * smooth);
+        }
+        else
+        {
+
+            transform.eulerAngles = Vector3.Slerp(transform.eulerAngles, defaultRot, Time.deltaTime * smooth);
+
+        }
+        if (Input.GetKeyDown(KeyCode.E) && enter)
+        {
+            open = !open;
+        }
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Player")
+        {
+            enter = true;
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if (col.tag == "Player")
+        {
+            enter = false;
+        }
+    }
+}
