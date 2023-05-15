7f9cd1cd6abe368055e84ce18f7026a164582501
Rybka <112930666+Rybk4@users.noreply.github.com>
2023-05-15 08:25:43 +0600

    21

---
+++ Assets/Script/Swords.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Swords : MonoBehaviour
+{
+
+    public GameObject Sword1;
+    public GameObject Sword2;
+    public GameObject Sword3;
+    public GameObject Sword4;
+
+    public GameObject Sword_Tron1;
+    public GameObject Sword_Tron2;
+    public GameObject Sword_Tron3;
+    public GameObject Sword_Tron4;
+
+    private Collider col;
+    private Collider col2;
+    private Collider col3;
+    private Collider col4;
+
+    void Start()
+    {
+         col = Sword1.GetComponent<Collider>();
+         col2 = Sword2.GetComponent<Collider>();
+         col3 = Sword3.GetComponent<Collider>();
+         col4 = Sword4.GetComponent<Collider>();
+    }
+
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other == col)
+        {
+           
+            Sword_Tron1.active = true;
+            Sword1.active = false;
+        }
+        else if (other == col2)
+        {
+         
+            Sword_Tron2.active = true;
+            Sword2.active = false;
+        }
+        else if (other == col3)
+        {
+           
+            Sword_Tron3.active = true;
+            Sword3.active = false;
+        }
+        else if(other == col4){
+
+            Sword_Tron4.active = true;
+            Sword4.active = false;
+        }
+    }
+     
+}
